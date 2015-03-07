85d556769700458e19c92036e98d57bb53d5af29
Charlin_冯成林 <muxi@muxideiMac.local>
2015-03-07 11:08:46 +0800

    Initial Commit

---
+++ CoreCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreCategory
+//
+//  Created by 沐汐 on 15-3-7.
+//  Copyright (c) 2015年 沐汐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreCategory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreCategory
+//
+//  Created by 沐汐 on 15-3-7.
+//  Copyright (c) 2015年 沐汐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
